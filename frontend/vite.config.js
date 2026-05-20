2072618df882d1bc9931d6f3dddc09a7b2fb129a
parshotamlal <parshotamworks@gmail.com>
2026-05-20 13:56:06 +0530

    add sitemap in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import generateSitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    generateSitemap({
+      hostname: 'https://www.resumeaionline.in'
+    })
+  ],
 })
